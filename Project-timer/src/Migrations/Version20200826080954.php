ee59cde8eee5dfaf3d6ba3729de809568d3024c4
Rodolphe de Scorraille <rodolphe@sixtrone.com>
2020-08-26 10:32:32 +0200

    fix: Timer features

--- Project-timer/src/Migrations/Version20200826080954.php
+++ Project-timer/src/Migrations/Version20200826080954.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200825134642 extends AbstractMigration
+final class Version20200826080954 extends AbstractMigration
 {
     public function getDescription() : string
     {
